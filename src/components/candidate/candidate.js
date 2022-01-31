4c25bb2984f5138c76b35154a89f4ba9a5a1b272
nturtanyr <nturnet@gmail.com>
2022-01-31 23:30:08 +0000

    Off it's a lot Added authentication stuff Can access user page Removed deprecated pages Removed modal for login Login and register kinda works now No forgot password tho Can confirm email

--- src/components/candidate/candidate.js
+++ src/components/candidate/candidate.js
@@ -1,23 +1,82 @@
 import React from "react";
 import axios from 'axios';
-import { useParams } from "react-router-dom"
-import ConstituencyElectionChart from "./electionresults";
+import CandidateInfo from "./candidateinfo";
 
 
-export default function Candidate() {
-    var params = useParams();
-    const [selected_index, setSelectedIndex] = React.useState(1)
+export default function Candidate({candidate_index}) {
+    const [selectedCandidate, setSelectedCandidate] = React.useState(candidate_index)
     const [candidateList, setCandidateList] = React.useState([]);
+    const [constituencyList, setConstituencyList] = React.useState([]);
 
     React.useEffect(() => {
-        axios.get(`${process.env.REACT_APP_API_ROOT}/candidate`)
+        axios.get(`${process.env.REACT_APP_API_ROOT}/candidates`)
         .then(res => {
             const candidateList = res.data.data;
             setCandidateList(candidateList);
         })
+
+        axios.get(`${process.env.REACT_APP_API_ROOT}/constituency`)
+        .then(res => {
+            const constituencyList = res.data.data;
+            setConstituencyList(constituencyList);
+        })
         
     },[]);
+    
+    var menu = []
+    if(constituencyList && candidateList){
+        constituencyList.forEach(constituency => {
+            var candidates = []
+            candidateList.forEach(candidate => {
+                if(candidate.constituency_id == constituency.id)
+                {
+                    var linkText = []
+                    if(candidate.elected)
+                    {
+                        linkText = <a onClick={() => setSelectedCandidate(candidate.candidate_id)}><i className="fas fa-star"></i>{candidate.first_name} {candidate.last_name}</a>
+                    }
+                    else if(candidate.running)
+                    {
+                        linkText = <a onClick={() => setSelectedCandidate(candidate.candidate_id)}>{candidate.first_name} {candidate.last_name}</a>
+                    }
+                    else
+                    {
+                        linkText = <a onClick={() => setSelectedCandidate(candidate.candidate_id)} className="has-text-grey-light"><i>{candidate.first_name} {candidate.last_name}</i></a>
+                    }
+                    candidates.push(<li key={`canlink-${candidate.candidate_id}`}>{linkText}</li>)
+                }
+            })
+            menu.push(
+            <li>
+                <a key={`conlink-${constituency.id}`} id={`constituency-${constituency.id}`} onClick={(event) => toggleMenu(event, constituency.id)}>{constituency.name}</a>
+                <ul className={`is-hidden ${constituency.id}`}>
+                    {candidates}
+                </ul>
+            </li>
+            )
+        })
+    }
 
+    function toggleMenu(event, constituency_id){
+        var elements = document.getElementsByClassName(constituency_id)
+        var link = document.getElementById("constituency-" + constituency_id)
+        if(elements){
+            if(link.classList.contains("is-active"))
+            {
+                link.classList.remove("is-active")
+                Array.from(elements).forEach( row => {
+                    row.classList.add('is-hidden');
+                    row.classList.remove('is-visible');
+                });
+            }else{
+                link.classList.add("is-active")
+                Array.from(elements).forEach( row => {
+                    row.classList.add('is-visible');
+                    row.classList.remove('is-hidden');
+                });
+            }
+        }
+    }
     return(
         <section className="section">
             <div className="content has-text-centered">
@@ -31,22 +90,12 @@ export default function Candidate() {
                             Constituencies
                         </p>
                         <ul className="menu-list">
-                        {
-                            conList.map(con =>
-                                <li key={`link-${con.id}`}><a onClick={() => setSelectedIndex(con.id)}>{con.name}</a></li>
-                            )
-                        }
+                        {menu}
                         </ul>
                     </aside>
                 </div>
                 <div className="column">
-                    <div className="content">
-                        { conList.length > 0 && (
-                            <h3>{conList[selected_index-1].name}</h3>
-                        )}
-                        <hr/>
-                    </div>
-                    <ConstituencyElectionChart constituency_id={selected_index} election_id="latest"/>
+                    <CandidateInfo candidate_id={selectedCandidate}/>
                 </div>
             </div>
         </section>
