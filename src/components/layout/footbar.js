389fd16d6878659c68ab0d0f286730b07d8300b3
nturtanyr <nturnet@gmail.com>
2022-03-26 23:45:21 +0000

    Updated footbar so Jenkins can replace version number

--- src/components/layout/footbar.js
+++ src/components/layout/footbar.js
@@ -5,7 +5,7 @@ function FooterBar() {
         <footer className="footer p-1">
             <div className="content is-small has-text-centered">
                 <i>No bears were hurt in the making of this site - so say us, the Kalmany Electoral Commission</i><br/>
-                {process.env.REACT_APP_} v{process.env.REACT_APP_VERSION} &#169; 2022 nturtaynment
+                kalmany-beetroot v{process.env.BUILD_DISPLAY_NAME} &#169; 2022 nturtaynment
             </div>    
         </footer>
     )
