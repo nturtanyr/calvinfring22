8758a5c6fa84aa3e9aa6c5f1c820fe7d394c872d
nturtanyr <nturnet@gmail.com>
2022-04-22 13:30:47 +0100

    Fixed some grammar in the homepage

--- src/components/home/constituencyinfo.js
+++ src/components/home/constituencyinfo.js
@@ -25,8 +25,8 @@ class ConstituencyInfo extends React.Component {
             image = null
             map = null;
             details = <div>
-                <p>The wonderous and insignificant country of Kalmany and it's citizens, the Kalmans, has existed for approximately two-hundred hours. They herald as a proud and faithful people, as well as incredibly predictable.</p>
-                <p>Believing so strongly in the political system, the Kalmans have elected that their parliament be formed every day, with new candidates standing and stepping down each day to provide a proper and thorough voice to the people, with no reasonable way to determine exactly what will pass and what will fail.</p>
+                <p>The wonderous and insignificant country of Kalmany and it's citizens, the Kalmans, have existed for approximately two-hundred hours. They herald as proud and faithful people, as well as incredibly predictable.</p>
+                <p>Believing so strongly in the political system, the Kalmans have dictated that their parliament be formed every day, with new candidates standing up and stepping down each day to provide a proper and thorough voice to the people, with no reasonable way to determine exactly what will pass and what will fail.</p>
                 <p>The Kalmans have a natural affinity for socks, bears, and political systems.</p>
             </div>
         }
